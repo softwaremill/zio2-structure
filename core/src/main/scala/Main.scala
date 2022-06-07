542c776fc8c4d737e63b0b47f89cba85c40baf9b
Kit Langton <kit.langton@gmail.com>
2022-06-07 09:23:48 -0400

    refactor Main

--- core/src/main/scala/Main.scala
+++ core/src/main/scala/Main.scala
@@ -1,21 +1,19 @@
 import zio.{Console, Scope, ZIO, ZIOAppDefault, ZLayer}
 
 object Main extends ZIOAppDefault:
-  override def run: ZIO[Any, Any, Any] =
-    ZIO.scoped {
-      ZLayer
-        .makeSome[Scope, CarApi](
-          CarApi.live,
-          CarService.live,
-          CarRepository.live,
-          DB.live,
-          ConnectionPool.live
-        )
-        .build
-        .map(_.get[CarApi])
-        .flatMap { api =>
-          api.register("Toyota Corolla WE98765").flatMap(Console.printLine(_)) *>
-            api.register("VW Golf WN12345").flatMap(Console.printLine(_)) *>
-            api.register("Tesla").flatMap(Console.printLine(_))
-        }
-    }
+  val program =
+    for {
+      api <- ZIO.service[CarApi]
+      _ <- api.register("Toyota Corolla WE98765").debug
+      _ <- api.register("VW Golf WN12345").debug
+      _ <- api.register("Tesla").debug
+    } yield ()
+
+  override def run =
+    program.provide(
+      CarApi.live,
+      CarService.live,
+      CarRepository.live,
+      DB.live,
+      ConnectionPool.live
+    )
