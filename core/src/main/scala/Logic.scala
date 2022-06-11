494fd1277fdef49082faf4420fcabfb7ce28277e
babis <haralampos.routis@intrasoft-intl.com>
2022-06-11 14:07:27 +0300

    Reworked the ConnectionPool layer to remove Scope. Simplified Main to express more clear the use case and the layer construction

--- core/src/main/scala/Logic.scala
+++ core/src/main/scala/Logic.scala
@@ -13,58 +13,57 @@ class CarApi(carService: CarService):
       input.split(" ", 3).toList match
         case List(f1, f2, f3) =>
           val car = Car(f1, f2, f3)
-          carService.register(car).map(_ => "OK: Car registered").catchAll {
+          carService.register(car).as("OK: Car registered").catchAll {
             case _: LicensePlateExistsError =>
               ZIO
                 .logError(
                   s"Cannot register: $car, because a car with the same license plate already exists"
                 )
-                .map(_ => "Bad request: duplicate")
+                .as("Bad request: duplicate")
             case t =>
               ZIO
                 .logErrorCause(s"Cannot register: $car, unknown error", Cause.fail(t))
-                .map(_ => "Internal server error")
+                .as("Internal server error")
           }
-        case _ => ZIO.logError(s"Bad request: $input").map(_ => "Bad Request")
+        case _ => ZIO.logError(s"Bad request: $input").as("Bad Request")
     }
 
 object CarApi:
-  lazy val live: ZLayer[CarService, Any, CarApi] = ZLayer.fromFunction(CarApi(_))
+  lazy val live: ZLayer[CarService, Nothing, CarApi] = ZLayer.fromFunction(CarApi(_))
+  def register(input: String): ZIO[CarApi, Nothing, String] =
+    ZIO.serviceWithZIO[CarApi](_.register(input))
 
 //
 
-class CarService(carRepository: CarRepository, db: DB):
+class CarService(db: DB):
   def register(car: Car): ZIO[Any, Throwable | LicensePlateExistsError, Unit] =
     db.transact {
-      carRepository.exists(car.licensePlate).flatMap {
+      CarRepository.exists(car.licensePlate).flatMap {
         case true  => ZIO.fail(LicensePlateExistsError(car.licensePlate))
-        case false => carRepository.insert(car)
+        case false => CarRepository.insert(car)
       }
     }
 
 object CarService:
-  lazy val live: ZLayer[CarRepository & DB, Nothing, CarService] =
-    ZLayer.fromFunction(CarService(_, _))
+  lazy val live: ZLayer[DB, Nothing, CarService] =
+    ZLayer.fromFunction(CarService(_))
 
 //
 
-class CarRepository():
+object CarRepository:
   def exists(licensePlate: String): ZIO[Connection, Nothing, Boolean] =
-    ZIO.sleep(100.millis) *>
-      ZIO
-        .service[Connection]
-        .map(_ => /* perform the check */ licensePlate.startsWith("WN"))
-        .tap(_ => ZIO.logInfo(s"Checking if license plate exists: $licensePlate"))
+    ZIO
+      .service[Connection]
+      .map(_ => /* perform the check */ licensePlate.startsWith("WN"))
+      .tap(_ => ZIO.logInfo(s"Checking if license plate exists: $licensePlate"))
+      .delay(100.millis)
 
   def insert(car: Car): ZIO[Connection, Nothing, Unit] =
-    ZIO.sleep(200.millis) *>
-      ZIO
-        .service[Connection]
-        .map(_ => /* perform the insert */ ())
-        .tap(_ => ZIO.logInfo(s"Inserting car: $car"))
-
-object CarRepository:
-  lazy val live: ZLayer[Any, Nothing, CarRepository] = ZLayer.succeed(CarRepository())
+    ZIO
+      .service[Connection]
+      .map(_ => /* perform the insert */ ())
+      .tap(_ => ZIO.logInfo(s"Inserting car: $car"))
+      .delay(200.millis)
 
 //
 
@@ -102,11 +101,13 @@ class ConnectionPool(r: Ref[Vector[Connection]]):
       .flatMap(conns => ZIO.foreachDiscard(conns)(conn => ZIO.logInfo(s"Closing: $conn")))
 
 object ConnectionPool:
-  lazy val live: ZLayer[Scope, Nothing, ConnectionPool] =
-    ZLayer(
-      ZIO.acquireRelease(
-        Ref
-          .make(Vector(Connection("conn1"), Connection("conn2"), Connection("conn3")))
-          .map(ConnectionPool(_))
-      )(_.close)
-    )
+
+  def create(size: Int): ZIO[Scope, Nothing, ConnectionPool] =
+    ZIO.acquireRelease(
+      Ref
+        .make(Vector.range(1, size).map(i => Connection(s"conn$i")))
+        .map(ConnectionPool(_))
+    )(_.close)
+
+  def live(size: Int): ZLayer[Any, Nothing, ConnectionPool] =
+    ZLayer.scoped(create(size))
--- core/src/main/scala/Main.scala
+++ core/src/main/scala/Main.scala
@@ -1,21 +1,21 @@
-import zio.{Console, Scope, ZIO, ZIOAppDefault, ZLayer}
+import zio.{Console, Scope, ZIO, ZIOAppDefault, ZLayer, URIO, IO}
 
 object Main extends ZIOAppDefault:
-  override def run: ZIO[Any, Any, Any] =
-    ZIO.scoped {
-      ZLayer
-        .makeSome[Scope, CarApi](
-          CarApi.live,
-          CarService.live,
-          CarRepository.live,
-          DB.live,
-          ConnectionPool.live
-        )
-        .build
-        .map(_.get[CarApi])
-        .flatMap { api =>
-          api.register("Toyota Corolla WE98765").flatMap(Console.printLine(_)) *>
-            api.register("VW Golf WN12345").flatMap(Console.printLine(_)) *>
-            api.register("Tesla").flatMap(Console.printLine(_))
-        }
-    }
+
+  private val program: URIO[CarApi, Unit] =
+    for
+      _ <- CarApi.register("Toyota Corolla WE98765").debug
+      _ <- CarApi.register("VW Golf WN12345").debug
+      _ <- CarApi.register("Tesla").debug
+    yield ()
+
+  private lazy val layer: ZLayer[Any, Nothing, CarApi] =
+    ZLayer.make[CarApi](
+      CarApi.live,
+      CarService.live,
+      DB.live,
+      ConnectionPool.live(3)
+    )
+
+  override def run: IO[Throwable, Unit] =
+    program.provide(layer)
