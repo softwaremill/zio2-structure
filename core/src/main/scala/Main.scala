a99c52be1c0ae6e53595a856725730263d253639
adamw <adam@warski.org>
2022-08-25 14:21:25 +0200

    Remove ZIO.scoped from Main

--- core/src/main/scala/Main.scala
+++ core/src/main/scala/Main.scala
@@ -3,23 +3,21 @@ import zio.{Console, Scope, ZIO, ZIOAppDefault, ZLayer}
 import java.io.IOException
 
 object Main extends ZIOAppDefault:
-  override def run: ZIO[Any, Any, Any] =
+  override def run: ZIO[Scope, Any, Any] =
     def program(api: CarApi): ZIO[Any, IOException, Unit] = for {
       _ <- api.register("Toyota Corolla WE98765").flatMap(Console.printLine(_))
       _ <- api.register("VW Golf WN12345").flatMap(Console.printLine(_))
       _ <- api.register("Tesla").flatMap(Console.printLine(_))
     } yield ()
 
-    ZIO.scoped {
-      ZLayer
-        .makeSome[Scope, CarApi](
-          CarApi.live,
-          CarService.live,
-          CarRepository.live,
-          DB.live,
-          ConnectionPool.live
-        )
-        .build
-        .map(_.get[CarApi])
-        .flatMap(program)
-    }
+    ZLayer
+      .makeSome[Scope, CarApi](
+        CarApi.live,
+        CarService.live,
+        CarRepository.live,
+        DB.live,
+        ConnectionPool.live
+      )
+      .build
+      .map(_.get[CarApi])
+      .flatMap(program)
